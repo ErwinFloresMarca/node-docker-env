b951d07ae6a7588b4d564c74dda148687da01aa4
Erwin Ramiro Flores Marca <ramiromarca6@gmail.com>
2022-07-27 09:45:42 -0400

    test with express server

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 require('dotenv').config()
 
-console.log('variable 1:', process.env.V_ENV_1);
-console.log('variable 2:', process.env.V_ENV_2);
-console.log('variable 3:', process.env.V_ENV_3);
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.status(200).json({
+    'variable 1:': process.env.V_ENV_1,
+    'variable 2:': process.env.V_ENV_2,
+    'variable 3:': process.env.V_ENV_3,
+  }).send();
+})
+
+app.listen(3000, (err) => {
+  if(!err)
+    console.log('Server is running in port: ', 3000);
+});
